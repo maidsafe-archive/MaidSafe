3163f42f868aab80a73a63925b3dcf112c0214b2
David Irvine <dirvine@di-ubuntu-64.(none)>
2012-05-03 16:01:42 +0100

    cmake updates

--- src/third_party_libs/cryptopp/aes.h
+++ src/third_party_libs/cryptopp/aes.h
@@ -6,7 +6,7 @@
 NAMESPACE_BEGIN(CryptoPP)
 
 //! <a href="http://www.cryptolounge.org/wiki/AES">AES</a> winner, announced on 10/2/2000
-DOCUMENTED_TYPEDEF(Rijndael, AES);
+DOCUMENTED_TYPEDEF(Rijndael, AES)
 
 typedef RijndaelEncryption AESEncryption;
 typedef RijndaelDecryption AESDecryption;
