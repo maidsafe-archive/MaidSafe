d5cde64f763e548fae0dd3b9612da6cc043416f8
David Irvine <david.irvine@maidsafe.net>
2012-05-03 22:45:14 +0100

    added remaining libraries to be adjusted for seperate include dirs

--- src/third_party_libs/cryptopp/zinflate.cpp
+++ src/third_party_libs/cryptopp/zinflate.cpp
@@ -402,7 +402,8 @@ void Inflator::DecodeHeader()
 			HuffmanDecoder codeLengthDecoder(codeLengths, 19);
 			for (i = 0; i < hlit+257+hdist+1; )
 			{
-				unsigned int k, count, repeater;
+				unsigned int k, repeater;
+                                unsigned int count = 0;
 				bool result = codeLengthDecoder.Decode(m_reader, k);
 				if (!result)
 					throw UnexpectedEndErr();
