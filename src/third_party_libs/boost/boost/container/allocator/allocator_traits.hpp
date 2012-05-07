47944fa7cbe2ba58b9306b987fa5cbf0df0446f8
David Irvine <david.irvine@maidsafe.net>
2012-05-07 22:08:14 +0100

    updates for PD to compile and run tests

--- src/third_party_libs/boost/boost/container/allocator/allocator_traits.hpp
+++ src/third_party_libs/boost/boost/container/allocator/allocator_traits.hpp
@@ -162,11 +162,11 @@ struct allocator_traits
          propagate_on_container_swap, boost::false_type)
             propagate_on_container_swap;
 
-      #if !defined(BOOST_NO_TEMPLATE_ALIASES)
+     /* #if !defined(BOOST_NO_TEMPLATE_ALIASES) // TODO(dirvine) this is broken in clang++
          //C++11
          template <typename T> using rebind_alloc  = boost::intrusive::detail::type_rebinder<Alloc, T>::type;
          template <typename T> using rebind_traits = allocator_traits< rebind_alloc<T> >;
-      #else    //!defined(BOOST_NO_TEMPLATE_ALIASES)
+      #else  */  //!defined(BOOST_NO_TEMPLATE_ALIASES)
          //Some workaround for C++03 or C++11 compilers with no template aliases
          template <typename T>
          struct rebind_alloc : boost::intrusive::detail::type_rebinder<Alloc,T>::type
@@ -193,7 +193,7 @@ struct allocator_traits
          struct rebind_traits
             : allocator_traits<typename boost::intrusive::detail::type_rebinder<Alloc, T>::type>
          {};
-      #endif   //!defined(BOOST_NO_TEMPLATE_ALIASES)
+//       #endif   //!defined(BOOST_NO_TEMPLATE_ALIASES)
       template <class T>
       struct portable_rebind_alloc
       {  typedef typename boost::intrusive::detail::type_rebinder<Alloc, T>::type type;  };
