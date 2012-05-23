1280526e2e565770d00ffc954b0129dfed033c6e
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2012-05-23 02:15:24 +0100

    Updated for MSVC11.

--- src/third_party_libs/googlemock/gtest/cmake/internal_utils.cmake
+++ src/third_party_libs/googlemock/gtest/cmake/internal_utils.cmake
@@ -17,29 +17,32 @@
 # This must be a macro(), as inside a function string() can only
 # update variables in the function scope.
 macro(fix_default_compiler_settings_)
-  if (MSVC)
-    # For MSVC, CMake sets certain flags to defaults we want to override.
-    # This replacement code is taken from sample in the CMake Wiki at
-    # http://www.cmake.org/Wiki/CMake_FAQ#Dynamic_Replace.
-    foreach (flag_var
-             CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
-             CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-      if (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt)
-        # When Google Test is built as a shared library, it should also use
-        # shared runtime libraries.  Otherwise, it may end up with multiple
-        # copies of runtime library data in different modules, resulting in
-        # hard-to-find crashes. When it is built as a static library, it is
-        # preferable to use CRT as static libraries, as we don't have to rely
-        # on CRT DLLs being available. CMake always defaults to using shared
-        # CRT libraries, so we override that default here.
-        string(REPLACE "/MD" "-MT" ${flag_var} "${${flag_var}}")
-      endif()
-
-      # We prefer more strict warning checking for building Google Test.
-      # Replaces /W3 with /W4 in defaults.
-      string(REPLACE "/W3" "-W4" ${flag_var} "${${flag_var}}")
-    endforeach()
-  endif()
+  include(${maidsafe_SOURCE_DIR}/cmake_modules/maidsafe_standard_flags.cmake)
+#   if (MSVC)
+#     # For MSVC, CMake sets certain flags to defaults we want to override.
+#     # This replacement code is taken from sample in the CMake Wiki at
+#     # http://www.cmake.org/Wiki/CMake_FAQ#Dynamic_Replace.
+#     foreach (flag_var
+#              CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+#              CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+#       if (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt)
+#       message("${flag_var} BEFORE - ${${flag_var}}")
+#         # When Google Test is built as a shared library, it should also use
+#         # shared runtime libraries.  Otherwise, it may end up with multiple
+#         # copies of runtime library data in different modules, resulting in
+#         # hard-to-find crashes. When it is built as a static library, it is
+#         # preferable to use CRT as static libraries, as we don't have to rely
+#         # on CRT DLLs being available. CMake always defaults to using shared
+#         # CRT libraries, so we override that default here.
+#         string(REPLACE "/MD" "/MT" ${flag_var} "${${flag_var}}")
+#       message("${flag_var} AFTER -  ${${flag_var}}")
+#       endif()
+# 
+#       # We prefer more strict warning checking for building Google Test.
+#       # Replaces /W3 with /W4 in defaults.
+#       string(REPLACE "/W3" "-W4" ${flag_var} "${${flag_var}}")
+#     endforeach()
+#   endif()
 endmacro()
 
 # Defines the compiler/linker flags used to build Google Test and
