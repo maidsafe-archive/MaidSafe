cd18ac46dac9afc99b256f96b25651856a6888ef
AlisonS <alison.shaw@maidsafe.net>
2012-11-23 15:20:21 +0000

    Added basis for JAV1 routing test. Code to be moved in to separate file later.

--- tools/dev.py
+++ tools/dev.py
@@ -4,6 +4,7 @@ import sys
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 import multiprocessing
+from time import sleep
 
 all = { "Common" : 'common', "Rudp" : 'rudp', "Routing" : 'routing',
         "Private" : 'private', "Pd" : 'pd', "Encrypt" : 'encrypt',
@@ -84,33 +85,100 @@ def RunNetwork(number_of_vaults):
   pool.map(work, [FindFile('TESTcommon', os.curdir)] * number_of_vaults)
 
 def RoutingSetupKeys(num):
-  p = Popen(['./routing_key_helper', '-c', '-n', num], shell = True, stdout=PIPE, stdin=PIPE)
+  p = subprocess.check_output('./routing_key_helper -c -p -n' + str(num), shell = True)
+  print "-----Keys helper output---------"
+  print p
+  print "-----END Keys helper output-----\n"
+
 
 def RoutingSetupBootstrap():
   p_b0 = Popen('./routing_node -s -b -i 0', shell = True, stdout=PIPE, stdin=PIPE)
   p_b1 = Popen('./routing_node -s -b -i 1', shell = True, stdout=PIPE, stdin=PIPE)
 
+  i = 0
+  while i < 50:
+    i = i + 1
+    next_line = p_b0.stdout.readline()
+    if next_line.find('Current BootStrap node endpoint info') != -1:
+      peer_0 = next_line.split()[7]
+      print('FOUND PEER', peer_0)
+      break
+
+  if i == 50:
+    print "Failed to set up bootstrap 1!"
+    return -1
+
+  i = 0
+  while i < 50:
+    i = i + 1
+    next_line = p_b1.stdout.readline()
+    if next_line.find('Current BootStrap node endpoint info') != -1:
+      peer_1 = next_line.split()[7]
+      print('FOUND PEER', peer_1)
+      break
+
+  if i == 50:
+    print "Failed to set up bootstrap 1!"
+    return -1
+
+  sleep(2)
+
+  p_b0.stdin.write('peer ' + peer_1 + '\n')
+  p_b1.stdin.write('peer ' + peer_0 + '\n')
+  sleep(5)
+  p_b0.stdin.write('zerostatejoin\n')
+  p_b0.stdin.write('zerostatejoin\n')
+  sleep(10)
+
+  return [peer_0, p_b0, p_b1]
+
+
 def Routing_JAV1():
   RoutingSetupKeys(20)
-  RoutingSetupBootstrap()
+  items = RoutingSetupBootstrap()
+  peer = items[0]
+  p_b0 = items[1]
+  p_b1 = items[2]
 
-  p_v = Popen('./routing_node -s -i 2', shell = True, stdout=PIPE, stdin=PIPE)
+  p_v = Popen('./routing_node -s -i 2 -p ' + peer, shell = True, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 
-  # check if "Current Node joined" get printed from p_v.stdout
-  p_v.stdin.write('exit\n')
-  # check the process doesn't run in backgroud
+  i = 0
+  while i < 100:
+    next_line = p_v.stdout.readline()
+    print 'p_v\t' + next_line.rstrip()
+    if next_line.find('Current Node joined') != -1:
+      print('NODE JOINED')
+      break
+    i = i + 1
+
+  if i == 100:
+    print "Node 2 failed to join!"
+    p_b0.stdin.write('exit\n')
+    p_b1.stdin.write('exit\n')
+    return -1
+
+#  p_v.stdin.write('exit\n')
+  sleep(10)
+  if p_v.poll() == None:
+    print "Failed to stop node 2!"
+  else:
+    print "Node 2 stopped successfully"
+
+#  p_b0.stdin.write('exit\n')
+#  p_b1.stdin.write('exit\n')
 
 def RunQaCheck():
+  Routing_JAV1()
 #  f = open('./temp_log.txt','w')
-  p = Popen('./routing_node -s -i 2 -p 192.168.0.131:14378', shell = True, stdout=PIPE, stdin=PIPE)
-  s = Popen('./routing_node -s -i 3 -p 192.168.0.131:14378', shell = True, stdout=PIPE, stdin=PIPE)
+#  p = Popen('./routing_node -s -i 2 -p 192.168.0.131:14378', shell = True, stdout=PIPE, stdin=PIPE)
+#  s = Popen('./routing_node -s -i 3 -p 192.168.0.131:14378', shell = True, stdout=PIPE, stdin=PIPE)
 #  while True:
 #    next_line = p.stdout.readline()
 #    if not next_line:
 #      break
 #    print next_line.rstrip()
 
-  p.stdin.write('help\n')
+#  p.stdin.write('help\n')
 
 #  while True:
 #    next_line = p.stdout.readline()
@@ -118,8 +186,8 @@ def RunQaCheck():
 #      break
 #    print next_line.rstrip()
 
-  p.stdin.write('exit\n')
-  s.stdin.write('exit\n')
+#  p.stdin.write('exit\n')
+#  s.stdin.write('exit\n')
 
 #time.sleep(2)
 #  p = Popen(['./routing_node', '-s -i 4 -p 192.168.0.129:61922'], shell=False, stdout=STDOUT, stdin=PIPE, stderr=STDOUT)
