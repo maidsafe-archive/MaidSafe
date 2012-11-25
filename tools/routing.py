58c2bc99f14c3e7fb5d025b9ce7734df5e8d973c
David Irvine <david.irvine@maidsafe.net>
2012-11-25 23:38:56 +0000

    updates for Qa audit suite

---
+++ tools/routing.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# Copyright (c) 2012 maidsafe.net limited
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#     * Neither the name of the maidsafe.net limited nor the names of its
+#     contributors may be used to endorse or promote products derived from this
+#     software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+import datetime
+import subprocess
+import utils
+from subprocess import Popen, PIPE, STDOUT
+from time import sleep
+
+
+def SetupKeys(num):
+  print("Setting up keys ... ")
+  return subprocess.call(['./routing_key_helper', '-c', '-p', '-n', str(num)], stdout=PIPE)
+
+
+def ExtractNodeEndpoint(p_b):
+  i = 0
+  line_limit = 50
+  t_start = datetime.datetime.now()
+  time_delta = datetime.datetime.now() - t_start
+  timeout = 30
+  while i < line_limit and time_delta < datetime.timedelta(seconds=timeout):
+    next_line = p_b.stdout.readline()
+    if next_line.find('Current BootStrap node endpoint info') != -1:
+      peer = next_line.split()[7]
+      return peer
+    i = i + 1
+    time_delta = datetime.datetime.now() - t_start
+
+  if i == line_limit or time_delta >= datetime.timedelta(seconds=timeout):
+    print "Failed to get endpoint"
+    return -1
+
+
+def SetupBootstraps():
+  p_b0 = Popen('./routing_node -s -b -i 0', shell = True, stdout=PIPE, stdin=PIPE)
+  p_b1 = Popen('./routing_node -s -b -i 1', shell = True, stdout=PIPE, stdin=PIPE)
+
+  peer_0 = ExtractNodeEndpoint(p_b0)
+  if peer_0 == -1:
+    KillBootstraps(p_b0, p_b1)
+    return -1
+
+  peer_1 = ExtractNodeEndpoint(p_b1)
+  if peer_1 == -1:
+    KillBootstraps(p_b0, p_b1)
+    return -1
+  sleep(1)
+
+  p_b0.stdin.write('peer ' + peer_1 + '\n')
+  p_b1.stdin.write('peer ' + peer_0 + '\n')
+
+  p_b0.stdin.write('zerostatejoin\n')
+  p_b1.stdin.write('zerostatejoin\n')
+  sleep(1)
+  print("Setup boostrap nodes ...")
+  return [peer_0, p_b0, p_b1]
+
+
+def StopBootstraps(p_b0, p_b1):
+  p_b0.stdin.write('exit\n')
+  p_b1.stdin.write('exit\n')
+
+
+def KillBootstraps(p_b0, p_b1):
+  # TODO - needs to kill all 4 processes. Currently only kills 2.
+  p_b0.kill()
+  p_b1.kill()
+
+def AddRoutingObject(peer):
+  p_v = Popen('./routing_node -s -i 2 -p ' + peer, shell = True, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+
+  i = 0
+  line_limit = 100
+  t_start = datetime.datetime.now()
+  time_delta = datetime.datetime.now() - t_start
+  timeout = 3
+  while i < line_limit and time_delta < datetime.timedelta(seconds=timeout):
+    next_line = p_v.stdout.readline()
+#    print 'p_v\t' + next_line.rstrip()
+    if next_line.find('Current Node joined') != -1:
+      break
+    i = i + 1
+    time_delta = datetime.datetime.now() - t_start
+
+  if i == line_limit or time_delta >= datetime.timedelta(seconds=timeout):
+    return -1
+  return p_v
+
+def JAV1():
+  if not SetupKeys(20) == 0:
+    return -1
+  items = SetupBootstraps()
+  if items == -1:
+    return -1
+  peer = items[0]
+  p_b0 = items[1]
+  p_b1 = items[2]
+
+  p_v = AddRoutingObject(peer)
+  if p_v == -1:
+    print 'Failed to add routing object!'
+    StopBootstraps(p_b0, p_b1)
+    return -1
+
+  p_v.stdin.write('exit\n')
+  sleep(2)
+  if p_v.poll() == None:
+    print "Failed to stop node 2!"
+    StopBootstraps(p_b0, p_b1)
+    return -1
+
+  StopBootstraps(p_b0, p_b1)
+  return 0
+
+def SanityCheck():
+  print("Running Routing Sanity Check, please wait ....")
+  if JAV1() == 0:
+    print 'Routing Sanity Check    : PASSED'
+  else:
+    print 'Routing Sanity Check    : FAILED'
+
+def main():
+  print("This is the suite for lifestuff Qa analysis")
+  SanityCheck()
+
+if __name__ == "__main__":
+  sys.exit(main())
+
---
+++ tools/utils.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python
+# Copyright (c) 2012 maidsafe.net limited
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#     * Neither the name of the maidsafe.net limited nor the names of its
+#     contributors may be used to endorse or promote products derived from this
+#     software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import os
+import sys
+import subprocess
+import multiprocessing
+import lifestuff_killer
+import routing
+import lifestuff
+import vault
+
+all = { "Common" : 'common', "Rudp" : 'rudp', "Routing" : 'routing',
+        "Private" : 'private', "Pd" : 'pd', "Encrypt" : 'encrypt',
+        "Drive" : 'drive', "Lifestuff" : 'lifestuff' }
+
+def CppLint():
+  encoding = sys.getfilesystemencoding()
+  script_path = os.path.dirname(unicode(__file__, encoding))
+  return os.path.join(script_path, 'cpplint.py')
+
+def YesNo(question, default="yes"):
+    valid = {"yes":True,   "y":True,  "ye":True,
+             "no":False,     "n":False}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' (or 'y' or 'n').\n")
+
+def GetProg(prog):
+  if os.name =='nt':
+    return os.path.join (os.curdir, utils.BuildType(), prog)
+  else:
+    return os.path.join (os.curdir, prog)
+
+
+def GetBuildDir():
+  if os.name == 'nt':
+    if FindFile('TESTcommon', os.path.join(os.curdir, 'Debug')) != None:
+      print("You seem to be running windows, found valid executibles in Debug")
+      if YesNo("Do you wish to use the Debug build for these tests"):
+        return os.path.join(os.curdir, Debug)
+    if FindFile('TESTcommon', os.path.join(os.curdir, 'Release')) != None:
+      print("You seem to be running windows, found valid executibles in Release")
+      if YesNo("Do you wish to use the Debug build for these tests"):
+        return os.path.join(os.curdir, Release)
+  else:
+    if FindFile('TESTcommon', os.path.join(os.curdir)) != None:
+      return os.path.join(os.curdir)
+  print("This does not look like a build directory, please run this from your build dir")
+  print("or build all project targets before running this tool")
+  return
+
+def ClearScreen():
+  os.system( [ 'clear', 'cls' ][ os.name == 'nt' ] )
+
+def GetLib():
+  option = ''
+  while option.lower() not in all.values() and option != 'q':
+    print ("Libraries available")
+    print ("-------------------")
+    for key, value in all.iteritems():
+      print (key)
+    print ("-------------------")
+    option = raw_input("please type library name (q to exit): ")
+  return option
+
+# style check 
+def StyleCheck():
+  option = GetLib()
+  if option == 'q':
+    ClearScreen()
+    return
+  directory = os.path.join(os.path.curdir, '..', 'src', option)
+  style_check = ""
+  for r,d,f in os.walk(directory):
+    for files in f:
+      if (files.endswith(".cc") or files.endswith(".h")) and not \
+         (files.endswith(".pb.cc") or files.endswith(".pb.h")):
+        style_check = os.path.join(r,files)
+        subprocess.call(['python', CppLint() , style_check])
+
+def BuildType():
+  with open(os.path.join(os.curdir, 'CMakeCache.txt')) as cmake_cache:
+    content = cmake_cache.readlines()
+  if any('CMAKE_BUILD_TYPE:STRING=Debug' in s for s in content):
+    return 'Debug'
+  if any('CMAKE_BUILD_TYPE:STRING=Release' in s for s in content):
+    return 'Release'
+
+def FindFile(name, path=None, exts=('','.exe')):
+  path = path or os.environ['PATH']
+  for dir in path.split(os.pathsep):
+    for ext in exts:
+      binpath = os.path.join(dir, name) + ext
+      if os.path.exists(binpath):
+        return os.path.abspath(binpath)
+  return None
+
+def CppCheck():
+  checker = FindFile('cppcheck')
+  if checker == None:
+    return
+  option = GetLib()
+  if option == 'q':
+    ClearScreen()
+    return
+  directory = os.path.join(os.path.curdir, '..', 'src', option)
+  style_check = ""
+  for r,d,f in os.walk(directory):
+    for files in f:
+      if (files.endswith(".cc") or files.endswith(".h")):
+        file_to_check = os.path.join(r,files)
+        subprocess.call([checker, file_to_check])
+  return checker
+
+def work(cmd):
+    return subprocess.call(cmd, shell=False)
+
+def RunNetwork(number_of_vaults):
+  pool = multiprocessing.Pool(processes=number_of_vaults)
+  pool.map(work, [FindFile('TESTcommon', os.curdir)] * number_of_vaults)
+
+
+def main():
+  ClearScreen()
+  build_dir = GetBuildDir()
+  if build_dir == None:
+    return -1;
+
+
+if __name__ == "__main__":
+      sys.exit(main())
