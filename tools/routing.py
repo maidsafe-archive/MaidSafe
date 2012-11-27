52ebce5b11feac5425ddc432a6c1d102339f60a6
mmoadeli <mmoadeli@maidsafe.net>
2012-11-27 00:58:58 +0000

    Implementing Join A Client 1

--- tools/routing.py
+++ tools/routing.py
@@ -112,6 +112,69 @@ def AddRoutingObject(peer):
     return -1
   return p_v
 
+def AddTypedRoutingObject(peer, index, node_type):
+  if node_type == 'c':
+    node_type = '-c'
+  p_v = Popen('./routing_node -s ' + node_type + ' -i ' + str(index) + ' -p ' + peer,
+              shell = True, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+
+  i = 0
+  line_limit = 100
+  t_start = datetime.datetime.now()
+  time_delta = datetime.datetime.now() - t_start
+  timeout = 3
+  while i < line_limit and time_delta < datetime.timedelta(seconds=timeout):
+    next_line = p_v.stdout.readline()
+#    print 'p_v\t' + next_line.rstrip()
+    if next_line.find('Current Node joined') != -1:
+      break
+    i = i + 1
+    time_delta = datetime.datetime.now() - t_start
+
+  if i == line_limit or time_delta >= datetime.timedelta(seconds=timeout):
+    return -1
+
+#  print 'added node # ' + str(index)
+  return p_v
+
+
+def AddRoutingObjects(peer, start, quantity, node_type):
+   p_s = []
+   for i in range(start, start + quantity):
+     result = AddTypedRoutingObject(peer, i, node_type)
+     if result == -1:
+       return -1
+     p_s.append(result)
+
+   return p_s
+
+def StopRoutingObjects(p_vs):
+  for p in p_vs:
+    p.stdin.write('exit\n')
+    sleep(2)
+    if p.poll() == None:
+      print "Failed to stop node!"
+
+def SendToDirect(p_c, index):
+  p_c.stdin.write('senddirect ' + str(index) + '\n')
+  i = 0
+  line_limit = 20
+  t_start = datetime.datetime.now()
+  time_delta = datetime.datetime.now() - t_start
+  timeout = 20
+  while i < line_limit and time_delta < datetime.timedelta(seconds=timeout):
+    next_line = p_c.stdout.readline()
+    if next_line.find('Response received in') != -1:
+      if ((next_line.split(' ')[-1]).split(':')[2]).find('20.') != -1:
+        return -1
+      print 'successfully sent to ' + str(index)
+      return 0
+    i = i + 1
+    time_delta = datetime.datetime.now() - t_start
+
+  return -1
+
+
 def JAV1():
   if not SetupKeys(20) == 0:
     return -1
@@ -138,9 +201,49 @@ def JAV1():
   StopBootstraps(p_b0, p_b1)
   return 0
 
+def JAC1():
+  if not SetupKeys(20) == 0:
+    return -1
+  items = SetupBootstraps()
+  if items == -1:
+    return -1
+  peer = items[0]
+  p_b0 = items[1]
+  p_b1 = items[2]
+
+  p_vs = AddRoutingObjects(peer, 2, 6, '')
+  if p_vs == -1:
+    print 'At lease one node among 6 nodes failed to start'
+    StopBootstrap(p_b0, p_b1)
+    return -1
+
+  p_c = AddTypedRoutingObject(peer, 11, 'c')
+  if p_c == -1:
+    print 'Failed to start client node'
+    StopBootstraps(p_b0, p_b1)
+    StopRoutingNodes(p_vs)
+    return -1
+  p_vs.append(p_c)
+  if SendToDirect(p_c, 11) != -1:
+#    print 'Client failed to send to self'
+    StopBootstraps(p_b0, p_b1)
+    StopRoutingObjects(p_vs)
+    return -1
+
+  if SendToDirect(p_c, 5) == -1:
+    print 'Client failed to send to vault'
+    StopBootstraps(p_b0, p_b1)
+    StopRoutingObjects(p_vs)
+    return -1
+
+  StopBootstraps(p_b0, p_b1)
+  StopRoutingObjects(p_vs)
+  return 0
+
+
 def SanityCheck():
   print("Running Routing Sanity Check, please wait ....")
-  if JAV1() == 0:
+  if (JAC1() == 0) and (JAV1() == 0):
     print 'Routing Sanity Check    : PASSED'
   else:
     print 'Routing Sanity Check    : FAILED'
@@ -151,4 +254,5 @@ def main():
 
 if __name__ == "__main__":
   sys.exit(main())
+
 
