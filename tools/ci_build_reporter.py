9bd4299434ac8b72faa1e6287488941c3e07c518
Viv-Rajkumar <viv.rajkumar@maidsafe.net>
2013-06-28 16:03:42 +0100

    Minor tweak to ci_build_reporter.py

--- tools/ci_build_reporter.py
+++ tools/ci_build_reporter.py
@@ -10,14 +10,7 @@ def main():
             'is_valid' : sys.argv[3],
             'project' : sys.argv[4],
             'committer' : sys.argv[5]}
-  requests.get("http://109.228.30.58/Submit.aspx", params=values)
-
-  # Send Mail Alert for Build Failure to dev@maidsafe.net
-  # server=smtplib.SMTP('smtp.gmail.com:587')
-  # server.starttls()
-  # server.login("ci@maidsafe.net","cipassw0rd")
-  # server.sendmail("ci@maidsafe.net", "dev@maidsafe.net", "Broken Project Details: {} && Broken by Details: {}")
-  # server.quit()
+  requests.get("http://buildstatus.novinet.com/Incoming/Submit.aspx", params=values)
 
 if __name__ == "__main__":
       main()
