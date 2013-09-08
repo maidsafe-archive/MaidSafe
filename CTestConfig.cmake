821cd4e6aae5544c94994a1d04115bb36b1da031
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2013-09-08 04:32:45 +0100

    License update, to allow greater developer freedom.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,18 +2,21 @@
 #                                                                                                  #
 #  Copyright 2012 MaidSafe.net limited                                                             #
 #                                                                                                  #
-#  This MaidSafe Software is licensed under the MaidSafe.net Commercial License, version 1.0 or    #
-#  later, and The General Public License (GPL), version 3. By contributing code to this project    #
-#  You agree to the terms laid out in the MaidSafe Contributor Agreement, version 1.0, found in    #
-#  the root directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also    #
-#  available at:                                                                                   #
+#  This MaidSafe Software is licensed to you under (1) the MaidSafe.net Commercial License,        #
+#  version 1.0 or later, or (2) The General Public License (GPL), version 3, depending on which    #
+#  licence you accepted on initial access to the Software (the "Licences").                        #
 #                                                                                                  #
-#    http://www.novinet.com/license                                                                #
+#  By contributing code to the MaidSafe Software, or to this project generally, you agree to be    #
+#  bound by the terms of the MaidSafe Contributor Agreement, version 1.0, found in the root        #
+#  directory of this project at LICENSE, COPYING and CONTRIBUTOR respectively and also available   #
+#  at: http://www.novinet.com/license                                                              #
 #                                                                                                  #
-#  Unless required by applicable law or agreed to in writing, software distributed under the       #
-#  License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,       #
-#  either express or implied. See the License for the specific language governing permissions      #
-#  and limitations under the License.                                                              #
+#  Unless required by applicable law or agreed to in writing, the MaidSafe Software distributed    #
+#  under the GPL Licence is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF   #
+#  ANY KIND, either express or implied.                                                            #
+#                                                                                                  #
+#  See the Licences for the specific language governing permissions and limitations relating to    #
+#  use of the MaidSafe Software.                                                                   #
 #                                                                                                  #
 #==================================================================================================#
 
