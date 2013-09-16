2b5dc4f7df18957f1d3fae948431b8c199ced7f2
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2013-09-16 10:06:57 +0100

    Changed CMake module to not require 2.8.12.

--- cmake_modules/maidsafe_find_git.cmake
+++ cmake_modules/maidsafe_find_git.cmake
@@ -36,7 +36,7 @@ if(Git_EXECUTABLE)
   execute_process(COMMAND ${Git_EXECUTABLE} --version RESULT_VARIABLE ResultVar OUTPUT_QUIET ERROR_QUIET)
   if(ResultVar EQUAL 0)
     # Set the path to git's folder so that CMake's built-in FindGit module can work if required later.
-    get_filename_component(GitFolder ${Git_EXECUTABLE} DIRECTORY)
+    get_filename_component(GitFolder ${Git_EXECUTABLE} PATH)
     list(APPEND CMAKE_SYSTEM_PREFIX_PATH ${GitFolder})
     return()
   endif()
