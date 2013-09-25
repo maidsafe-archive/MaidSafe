63caca83a4c5a40ec79264023d3a9ba90db55687
David Irvine <david.irvine@maidsafe.net>
2013-09-25 17:34:37 +0100

    update for swap

--- tools/cpplint.py
+++ tools/cpplint.py
@@ -3548,7 +3548,7 @@ _HEADERS_CONTAINING_TEMPLATES = (
     ('<set>', ('set', 'multiset',)),
     ('<stack>', ('stack',)),
     ('<string>', ('char_traits', 'basic_string',)),
-    ('<utility>', ('pair',)),
+    ('<utility>', ('pair', 'std::swap')),
     ('<vector>', ('vector',)),
 
     # gcc extensions.
@@ -3561,8 +3561,7 @@ _HEADERS_CONTAINING_TEMPLATES = (
 _RE_PATTERN_STRING = re.compile(r'\bstring\b')
 
 _re_pattern_algorithm_header = []
-for _template in ('copy', 'max', 'min', 'min_element', 'sort', 'swap',
-                  'transform'):
+for _template in ('copy', 'max', 'min', 'min_element', 'sort', 'transform'):
   # Match max<type>(..., ...), max(..., ...), but not foo->max, foo.max or
   # type::max().
   _re_pattern_algorithm_header.append(
