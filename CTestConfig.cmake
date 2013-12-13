9939d1ec24fab8724a66b9ce8c67ef726f37c609
Andrew Leung <andrewleung1@maidsafe.net>
2013-12-13 11:34:23 +0000

    Changed nightly start time

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -22,7 +22,7 @@
 
 
 set(CTEST_PROJECT_NAME "maidsafe")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
+set(CTEST_NIGHTLY_START_TIME "00:01:00 UTC")
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "dash.maidsafe.net")
 set(CTEST_DROP_LOCATION "/submit.php?project=MaidSafe")
