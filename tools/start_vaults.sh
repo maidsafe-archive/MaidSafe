4d13f78cd707a34cd9e0a9ba8c003524791f5422
Qi Ma <qi.ma@maidsafe.net>
2014-01-24 16:48:09 +0000

    include the script start vault in separate terminal

--- tools/start_vaults.sh
+++ tools/start_vaults.sh
@@ -7,8 +7,9 @@ echo "Setting up $vault_count vaults..."
 i=4
 while [ "$i" -le "$vault_count" ]
 do
-	~/MaidSafe-Open/build/$exe --log_no_async true --log_private V --log_passport V --log_nfs V --log_vault V --log_* Error --identity_index $i &>vault_$i.txt &
+	~/MaidSafe-Open/build/$exe --log_no_async true --log_routing V --log_private V --log_passport V --log_nfs V --log_vault V --log_* E --identity_index $i &>vault_$i.txt &
 	echo "Vault using key $i started."
+# x-terminal-emulator -e /home/maidsafe/MaidSafe-Open/build/$exe --identity_index $i
 	i=$(( $i + 1 ))
         sleep 1
 done
