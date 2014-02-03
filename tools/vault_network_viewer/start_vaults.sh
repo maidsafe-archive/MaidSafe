a240dc13f7f37be6dd20814eaf4be39a80f7dda1
Qi Ma <qi.ma@maidsafe.net>
2014-02-03 11:09:37 +0000

    include the command start up vaults with output to kGraph

--- tools/vault_network_viewer/start_vaults.sh
+++ tools/vault_network_viewer/start_vaults.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 exe=vault
-vault_count=60
+vault_count=40
 
 echo "Setting up $vault_count vaults..."
 i=4
 while [ "$i" -le "$vault_count" ]
 do
-	~/MaidSafe-Open/build/$exe --log_no_async true --log_routing V --log_private V --log_passport V --log_nfs V --log_vault V --log_* E --identity_index $i &>vault_$i.txt &
+#	~/MaidSafe-Open/build/$exe --log_no_async true --log_routing V --log_private V --log_passport V --log_nfs V --log_vault V --log_* E --identity_index $i &>vault_$i.txt &
+	~/MaidSafe-Open/build/$exe --log_* G --identity_index $i &>vault_$i.txt &
 	echo "Vault using key $i started."
-# x-terminal-emulator -e /home/maidsafe/MaidSafe-Open/build/$exe --identity_index $i
+#       x-terminal-emulator -e /home/maidsafe/MaidSafe-Open/build/$exe --identity_index $i
 	i=$(( $i + 1 ))
         sleep 1
 done
