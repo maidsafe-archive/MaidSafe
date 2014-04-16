2796b6ebaa58630471dd50680331176c83cf97cc
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2014-04-16 12:14:21 +0100

    Removed dependency on Python libraries.

--- cmake_modules/maidsafe_find_python.cmake
+++ cmake_modules/maidsafe_find_python.cmake
@@ -21,21 +21,8 @@
 #==================================================================================================#
 
 
-if(NOT PYTHON_EXECUTABLE OR NOT PYTHON_LIBRARY)
-  message("${HR}")
-endif()
 set(Python_ADDITIONAL_VERSIONS 3.9 3.8 3.7 3.6 3.5 3.4 3.3 3.2 3.1 3.0)
 find_package(PythonInterp REQUIRED)
 if(NOT PYTHONINTERP_FOUND)
   message(FATAL_ERROR "Didn't find python executable.")
 endif()
-if(WIN32)
-  get_filename_component(PythonPath ${PYTHON_EXECUTABLE} PATH)
-  get_filename_component(PythonLibName ${PythonPath} NAME)
-  string(TOLOWER "${PythonLibName}" PythonLibName)
-  find_library(PythonLibrary NAMES ${PythonLibName} PATHS ${PythonPath}/libs NO_DEFAULT_PATH)
-  find_path(PythonIncludeDir NAMES Python.h PATHS ${PythonPath}/include NO_DEFAULT_PATH)
-  set(PYTHON_LIBRARY "${PythonLibrary}")
-  set(PYTHON_INCLUDE_DIR "${PythonIncludeDir}")
-endif()
-find_package(PythonLibs REQUIRED)
